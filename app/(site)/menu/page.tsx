47eccf4ab287feb2754aa19c7d4808f2106c9867
amanbhatnagar2013 <amanbhatnagar2013@gmail.com>
2025-10-08 12:38:35 +0530

    Add files via upload

--- app/(site)/menu/page.tsx
+++ app/(site)/menu/page.tsx
@@ -1,10 +1,11 @@
-import menu from "@/data/menu.json";
+import menu from "../../data/menu.json";
+
 export default function MenuPage() {
   return (
     <div className="container py-10">
       <h1 className="text-3xl font-bold mb-6">Café Menu</h1>
       <div className="grid md:grid-cols-2 gap-4">
-        {menu.map((m,i) => (
+        {(menu as any[]).map((m:any,i:number) => (
           <div key={i} className="card flex justify-between">
             <div>{m.item} {m.veg ? "🟢" : "🔴"}</div>
             <div>₹{m.price}</div>
